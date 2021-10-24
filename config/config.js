b993f07fd60834fe064a1073c2750e0e3d8dd1eb
leubenia <hdodo777@gmail.com>
2021-10-24 13:49:32 +0900

    약간 수정

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
-{
+require("dotenv").config();
+module.exports ={
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_END_POINT,
     "dialect": "mysql"
   },
   "test": {
