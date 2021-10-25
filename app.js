91c8ed7549c223b1bd123b1502b81ce3a2034cf1
bohoon Lee <bohoon100@gmail.com>
2021-10-25 15:09:20 +0900

    [docs]일부 수정

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const cors = require('cors');
+const cors = require("cors");
 // const swaggerUi = require('swagger-ui-express'); //스웨거 자동생성을 위한 코드
 // const swaggerFile = require('./swagger_output.json'); //스웨거 아웃풋파일 저장 위치
 
@@ -11,25 +11,24 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const Router = require('./routers');
-app.use('/api', [Router]);
+const Router = require("./routers");
+app.use("/", [Router]);
 
 // app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-const { sequelize, Sequelize } = require('./models');
+const { sequelize, Sequelize } = require("./models");
 
 const driver = async () => {
   try {
     await sequelize.sync();
   } catch (err) {
-    console.error('초기화 실패');
+    console.error("초기화 실패");
     console.error(err);
     return;
   }
-  console.log('초기화 완료.');
-}
+  console.log("초기화 완료.");
+};
 
-driver();
-
+//driver();
 
 module.exports = app;
