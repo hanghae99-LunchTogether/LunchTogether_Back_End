ee92371a796ff95b3ceb5189ee0e7982470e321c
bohoon Lee <bohoon100@gmail.com>
2021-10-25 16:59:02 +0900

    to pull

--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const userRouter = require("./user");
 const commRouter = require("./comment");
+const lunchRouter = require("./lunchpost");
 router.use("/", [userRouter]);
 router.use("/comment", [commRouter]);
+router.use("/lunchpost", [lunchRouter]);
 
 module.exports = router;
