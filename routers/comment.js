9e8b149b6e3e302c7bd62115828aa93ade0e468b
changwoo Jeong <jcw9828@gmail.com>
2021-10-27 00:24:52 +0900

    [docs]미들웨어통일

--- routers/comment.js
+++ routers/comment.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const middleware = require("../middlewares/authMiddleware");
+const authmiddleware = require("../middlewares/authMiddleware");
 const { commentget, commentpost } = require("../controller/comment");
 
 router
   .route("/:lunchid")
-  .get(middleware, commentget)
-  .post(middleware, commentpost);
-router.route("/:commentid").delete(middleware, commentdele);
+  .get(authmiddleware, commentget)
+  .post(authmiddleware, commentpost);
+router.route("/:commentid").delete(authmiddleware, commentdele);
 
 module.exports = router;
