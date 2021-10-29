a066ad50150397cec099517af7aeec3f9d969dab
bohoon Lee <bohoon100@gmail.com>
2021-10-29 11:04:19 +0900

    df

--- app.js
+++ app.js
@@ -3,18 +3,28 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 
-const cors = require('cors');
-const swaggerUi = require('swagger-ui-express'); //스웨거 자동생성을 위한 코드
-const swaggerFile = require('./swagger_output.json'); //스웨거 아웃풋파일 저장 위치
+const kakaoLoginRouter = require("./routers/kakaologin.js");
+
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express"); //스웨거 자동생성을 위한 코드
+const swaggerFile = require("./swagger_output.json"); //스웨거 아웃풋파일 저장 위치
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
 const Router = require("./routers");
 app.use("/", [Router]);
+app.use("/kakao", [kakaoLoginRouter]);
 
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.get("/kakao", (req, res, next) => {
+  res.render("kakaologin");
+});
+
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 const { sequelize, Sequelize } = require("./models");
 
