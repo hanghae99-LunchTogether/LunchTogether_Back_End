c4676aaba58ad4c3db4eee169f922aed9e9e0be8
leubenia <hdodo777@gmail.com>
2021-11-09 13:12:59 +0900

    [fix] test

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const cors = require("cors");
 const swaggerUi = require("swagger-ui-express"); //스웨거 자동생성을 위한 코드
 const swaggerFile = require("./swagger_output.json"); //스웨거 아웃풋파일 저장 위치
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: 'https://lunchtogether-88cf5.web.app/', credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
