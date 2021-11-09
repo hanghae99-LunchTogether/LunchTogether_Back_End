06c01bbe63fdf8d3cb7e0ac79b76238cd947afb7
leubenia <hdodo777@gmail.com>
2021-11-09 21:33:54 +0900

    test

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// const { app, sessionMiddleware } = require("./app");
-// const port = process.env.EXPRESS_PORT;
+const { app, sessionMiddleware } = require("./app");
+const port = process.env.EXPRESS_PORT;
 // const webSocket = require("./soket");
 // const http =require('http');
 // const https = require('https')
@@ -23,42 +23,14 @@
 
 // webSocket(server, app, sessionMiddleware);
 
-// const express = require("express");
-// const http = require("http");
-// const https = require("https");
-// const fs = require("fs");
-// // const HTTP_PORT = 3000;
-// const HTTPS_PORT = 3000;
-// const options = {
-//   ca: fs.readFileSync("/etc/letsencrypt/live/lebania.shop/fullchain.pem"),
-//   key: fs.readFileSync("/etc/letsencrypt/live/lebania.shop/privkey.pem"),
-//   cert: fs.readFileSync("/etc/letsencrypt/live/lebania.shop/cert.pem"),
-// };
-// const app = express(); // Default route for server status
-// app.get("/", (req, res) => {
-//   res.json({
-//     message: `Server is running on port ${req.secure ? HTTPS_PORT : HTTP_PORT}`,
-//   });
-// }); 
-// // Create an HTTP server. 
-// // http.createServer(app).listen(HTTP_PORT); 
-// // Create an HTTPS server. 
-// https.createServer(options, app).listen(HTTPS_PORT);
-
-
-
-
-//   ca: fs.readFileSync("/etc/letsencrypt/live/lebania.shop/fullchain.pem"),
-//   key: fs.readFileSync("/etc/letsencrypt/live/lebania.shop/privkey.pem"),
-//   cert: fs.readFileSync("/etc/letsencrypt/live/lebania.shop/cert.pem"),
 'use strict';
 const fs = require("fs");
 const http = require("http");
 const https = require("https");
 const express = require("express");
 
-const app = express();  // https
-const app2 = express();  // http
+// const app = express();  // https
+// const app2 = express();  // http
 
 // yourdomain.com 은 실제로 사용중인 도메인으로 바꿔야함. -- Let's Encrypt 인증서 설치시 위치 사용.
 const privateKey = fs.readFileSync("/etc/letsencrypt/live/lebania.shop/privkey.pem", "utf8");
@@ -71,36 +43,36 @@ const credentials = {
     ca: ca
 };
 
-const httpServer = http.createServer(app2);
+// const httpServer = http.createServer(app2);
 const httpsServer = https.createServer(credentials, app);
 
-// 80 port -- http
-app2.get("/", (req, res) => {
-    console.log("------ http get / -----" + (new Date()).toLocaleString());
-    console.log("req.ip => " + req.ip);
-    console.log("req.hostname => " + req.hostname);
-    console.log(req.url);
-    console.log(req.originalUrl);
+// // 80 port -- http
+// app2.get("/", (req, res) => {
+//     console.log("------ http get / -----" + (new Date()).toLocaleString());
+//     console.log("req.ip => " + req.ip);
+//     console.log("req.hostname => " + req.hostname);
+//     console.log(req.url);
+//     console.log(req.originalUrl);
 
-    res.send("<h1>HTTP Server running on port 80</h1>");
-})
+//     res.send("<h1>HTTP Server running on port 80</h1>");
+// })
 
-// 3000 port -- https
-app.get("/", (req, res) => {
-    console.log("------ https get / -----" + (new Date()).toLocaleString());
-    console.log("req.ip => " + req.ip);
-    console.log("req.hostname => " + req.hostname);
-    console.log(req.url);
-    console.log(req.originalUrl);
+// // 3000 port -- https
+// app.get("/", (req, res) => {
+//     console.log("------ https get / -----" + (new Date()).toLocaleString());
+//     console.log("req.ip => " + req.ip);
+//     console.log("req.hostname => " + req.hostname);
+//     console.log(req.url);
+//     console.log(req.originalUrl);
 
-    res.send("<h1>HTTPS Server running on port 3000</h1>");
-})
+//     res.send("<h1>HTTPS Server running on port 3000</h1>");
+// })
 
 
-httpServer.listen(80, () => {
-    console.log((new Date()).toLocaleString());
-    console.log('HTTP Server running on port 80');
-})
+// httpServer.listen(80, () => {
+//     console.log((new Date()).toLocaleString());
+//     console.log('HTTP Server running on port 80');
+// })
 
 httpsServer.listen(3000, ()=>{
     console.log((new Date()).toLocaleString());
