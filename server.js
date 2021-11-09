be5babd7b361fb566929c0925c2e7b46d0d0ba64
leubenia <hdodo777@gmail.com>
2021-11-09 17:15:14 +0900

    [fix]테스트

--- server.js
+++ server.js
@@ -3,8 +3,28 @@ const port = process.env.EXPRESS_PORT;
 const webSocket = require('./soket');
 
 
-const server = app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`);
+const https = require('https')
+const fs = require('fs')
+
+const options = { 
+    key: fs.readFileSync('./rootca.key'), 
+    cert: fs.readFileSync('./rootca.crt') 
+  };
+  
+
+
+// https.createServer(options, (req, res) => {
+//     res.writeHead(200);
+//     res.end('hello world\n');
+// }).listen(8000);
+
+https.createServer(options, app).listen(3000, function() {
+    console.log("HTTPS server listening on port " + 3000);
 });
 
-webSocket(server, app, sessionMiddleware)
+
+// const server = app.listen(port, () => {
+//     console.log(`listening at http://localhost:${port}`);
+// });
+
+// webSocket(server, app, sessionMiddleware)
