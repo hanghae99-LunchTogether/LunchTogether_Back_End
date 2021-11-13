3cac1bd289f5a86c18359e70aac1ab7a2a04dcc9
leubenia <hdodo777@gmail.com>
2021-11-13 12:19:27 +0900

    [feat] 런치겟 페이징

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ httpServer.listen(80, () => {
   console.log("HTTP Server running on port 80");
 });
 
-// const server = httpsServer.listen(443, ()=>{
-//     console.log((new Date()).toLocaleString());
-//     console.log(`HTTPS -- listening on port 443 ...`);
-// })
+const server = httpsServer.listen(443, ()=>{
+    console.log((new Date()).toLocaleString());
+    console.log(`HTTPS -- listening on port 443 ...`);
+})
 
-// webSocket(server, app, sessionMiddleware);
+webSocket(server, app, sessionMiddleware);
 
 // webSocket(server, app, sessionMiddleware);
 
