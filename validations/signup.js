da9b67b2d22049aa34a86169660d30a527aa2456
bohoon Lee <bohoon100@gmail.com>
2021-11-24 13:40:30 +0900

    [fix]validations 수정

--- validations/signup.js
+++ validations/signup.js
@@ -1,15 +1,15 @@
 import Joi from "joi";
 
-const usernameRegExp = /^[0-9a-z+]{3,8}/;
-const passwordRegExp = /^[0-9a-z]{8,16}/;
+const usernameRegExp = /^[0-9a-zㄱ-ㅎㅏ-ㅣ가-힣]$/gi;
+const passwordRegExp =
+  /^[0-9a-z!?@#$%^&*():;+-=~{}<>\_\[\]\|\\\"\'\,\.\/\`\₩]$/gi;
 
 const signUpSchema = Joi.object({
-  nickname: Joi.string().regex(usernameRegExp).required(),
+  nickname: Joi.string().min(3).max(8).regex(usernameRegExp).required(),
   email: Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
     .required(),
-  password: Joi.string().regex(passwordRegExp).required(),
-  confirmPassword: Joi.string().regex(passwordRegExp).required(),
+  password: Joi.string().min(8).max(16).regex(passwordRegExp).required(),
 });
 
 export default signUpSchema;
