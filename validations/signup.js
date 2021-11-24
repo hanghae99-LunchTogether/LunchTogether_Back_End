e81c83a6c21182704f7f2a4bc882be7fcef4949e
leubenia <hdodo777@gmail.com>
2021-11-24 19:23:18 +0900

    [fix] 유저 프로필 파이어베이스

--- validations/signup.js
+++ validations/signup.js
@@ -1,8 +1,8 @@
 const Joi = require("joi");
 
-const usernameRegExp = /^[0-9a-zㄱ-ㅎㅏ-ㅣ가-힣]$/gi;
+const usernameRegExp = /^[0-9a-zㄱ-ㅎㅏ-ㅣ가-힣]$/i;
 const passwordRegExp =
-  /^[0-9a-z!?@#$%^&*():;+-=~{}<>\_\[\]\|\\\"\'\,\.\/\`\₩]$/gi;
+  /^[0-9a-z!?@#$%^&*():;+-=~{}<>\_\[\]\|\\\"\'\,\.\/\`\₩]$/i;
 
 const signUpSchema = Joi.object({
   nickname: Joi.string().min(3).max(8).regex(usernameRegExp).required(),
