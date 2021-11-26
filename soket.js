c5dd66a6b756bc3436ab09965e99d76a9923df45
leubenia <hdodo777@gmail.com>
2021-11-26 18:30:58 +0900

    test

--- soket.js
+++ soket.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 const cookieParser = require('cookie-parser');
 const cookie = require('cookie-signature');
 
-onlineMap = {};
 module.exports = (server, app, sessionMiddleware) => {
   const io = SocketIO(server, { path: '/socket.io' });
   app.set('io', io);
@@ -67,30 +66,4 @@ module.exports = (server, app, sessionMiddleware) => {
     });
   });
 
-  app.set("onlineMap", onlineMap);
-  const dynamicNsp = io.of(/^\/ws-.+$/).on("connect", (socket) => {
-    const newNamespace = socket.nsp; // newNamespace.name === '/dynamic-101'
-    if (!onlineMap[socket.nsp.name]) {
-      onlineMap[socket.nsp.name] = {};
-    }
-    // broadcast to all clients in the given sub-namespace
-    socket.emit("hello", socket.nsp.name);
-    socket.on("login", ({ id, channels }) => {
-      onlineMap[socket.nsp.name][socket.id] = id;
-      newNamespace.emit(
-        "onlineList",
-        Object.values(onlineMap[socket.nsp.name])
-      );
-      channels.forEach((channel) => {
-        socket.join(`${socket.nsp.name}-${channel}`);
-      });
-    });
-    socket.on("disconnect", () => {
-      delete onlineMap[socket.nsp.name][socket.id];
-      newNamespace.emit(
-        "onlineList",
-        Object.values(onlineMap[socket.nsp.name])
-      );
-    });
-  });
 };
