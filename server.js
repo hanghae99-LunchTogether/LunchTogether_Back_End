dc624314a2f967f3854a1bf07d658f7328cb6de7
leubenia <hdodo777@gmail.com>
2021-11-26 18:54:38 +0900

    test

--- server.js
+++ server.js
@@ -2,34 +2,50 @@ const { app, sessionMiddleware } = require("./app");
 const webSocket = require("./soket");
 const port = process.env.EXPRESS_PORT;
 ("use strict");
-const fs = require("fs");
-const http = require("http");
-const https = require("https");
 
-const privateKey = fs.readFileSync("/etc/letsencrypt/live/lebania.shop/privkey.pem", "utf8");
-const certificate = fs.readFileSync("/etc/letsencrypt/live/lebania.shop/cert.pem", "utf8")
-const ca = fs.readFileSync("/etc/letsencrypt/live/lebania.shop/fullchain.pem", "utf8")
 
-const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca
-};
+if(!process.env.TEST_PORT){
+  const fs = require("fs");
+  const http = require("http");
+  const https = require("https");
 
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+  const privateKey = fs.readFileSync("/etc/letsencrypt/live/lebania.shop/privkey.pem", "utf8");
+  const certificate = fs.readFileSync("/etc/letsencrypt/live/lebania.shop/cert.pem", "utf8")
+  const ca = fs.readFileSync("/etc/letsencrypt/live/lebania.shop/fullchain.pem", "utf8")
 
-const test = httpServer.listen(80, () => {
-  console.log(new Date().toLocaleString());
-  console.log("HTTP Server running on port 80");
-});
+  const credentials = {
+      key: privateKey,
+      cert: certificate,
+      ca: ca
+  };
 
-const server = httpsServer.listen(443, ()=>{
-    console.log((new Date()).toLocaleString());
-    console.log(`HTTPS -- listening on port 443 ...`);
-})
+  const httpServer = http.createServer(app);
+  const httpsServer = https.createServer(credentials, app);
 
-webSocket(server, app, sessionMiddleware);
+  const test = httpServer.listen(80, () => {
+    console.log(new Date().toLocaleString());
+    console.log("HTTP Server running on port 80");
+  });
+
+  const server = httpsServer.listen(443, ()=>{
+      console.log((new Date()).toLocaleString());
+      console.log(`HTTPS -- listening on port 443 ...`);
+  })
+
+  webSocket(server, app, sessionMiddleware);
+}else{
+  const httpServer = http.createServer(app);
+  const test = httpServer.listen(80, () => {
+    console.log(new Date().toLocaleString());
+    console.log("HTTP Server running on port 80");
+  });
+  webSocket(test, app, sessionMiddleware);
+}
+
+
+
+
+
 
 //혹시모를 예전 서버코드
 // const webSocket = require("./soket");
