d49a290745aeabc1999e11425bdd3e5aa4172ea1
leubenia <hdodo777@gmail.com>
2021-11-29 20:47:19 +0900

    test

--- middlewares/passportmid.js
+++ middlewares/passportmid.js
@@ -10,7 +10,7 @@ exports.isNotLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     next();
   } else {
-    console.log(req.user);
+    console.log(req);
     // const message = encodeURIComponent("로그인한 상태입니다.");
     res.status(200).send({ msg: "로그인 되있음요~!", user: req.user });
     // res.status(200).send({
