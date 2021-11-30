26a7460fe408861c7f2d0bae968b794fd4c55f83
leubenia <hdodo777@gmail.com>
2021-11-30 21:20:04 +0900

    test

--- routers/offer.js
+++ routers/offer.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
+const passmid = require('../middlewares/passportmid')
 const { postlunchlist ,offerconfirmed, test, dotest} = require("../controller/offeruser");
 
 router.route("/:userid").post(authMiddleware, postlunchlist);
 router.route("/confirmed/:lunchid").patch(authMiddleware, offerconfirmed);
 router.route("/").get(test);
-router.route("/test").post(dotest)
+router.route("/test").post(passmid.isLoggedIn, dotest)
 
 module.exports = router;
