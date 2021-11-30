d5308333ddfbf7fddfd90e58168ad51f07258154
leubenia <hdodo777@gmail.com>
2021-11-30 15:58:30 +0900

    test

--- soket.js
+++ soket.js
@@ -38,7 +38,8 @@ module.exports = (server, app, sessionMiddleware) => {
   const test = io.of('/userin');
   test.use(ios(sessionMiddleware, { autoSave:true }));
   test.on('connection', (socket) => {
-    socket.emit("message","서버에서 메세지");
+    console.log("클라이언트 연결")
+    // socket.emit("message","서버에서 메세지");
     socket.on('join', (massage) => {
       console.log(massage);
       // redisClient.hset("inneruser",socket.handshake.session.passport.user)
