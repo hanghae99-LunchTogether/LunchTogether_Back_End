5e99919d3d8a53dede46bf3e84a82b465138219d
leubenia <hdodo777@gmail.com>
2021-12-01 22:35:01 +0900

    TEST

--- controller/offeruser.js
+++ controller/offeruser.js
@@ -233,7 +233,7 @@ dotest = async (req, res)=>{
         userid: userid,
         kind : kind,
         message : "테스트 알림입니다..!",
-        nickname : req.user.nickname
+        nickname : sender
       })
       // req.app.get('io').of('/userin').to(data).emit('offer', offerdata);
       return res.status(200).send("잘되네요~!")
